1afbaaaf4170be98d0fc190a50dd9bf3d625af89
Sparkles <smcgarr13@yahoo.com>
2023-03-30 17:00:02 -0500

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,35 @@
->> main
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306,
-//     },
-//   );
-// }
-
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
